c7d0d4a38415204a33aeeb6fa1256c2e233964fc
jnpco <jnpco.98@gmail.com>
2019-03-04 11:32:09 +1100

    Added shuffle utility class.

---
+++ src/Utility.js
@@ -0,0 +1,12 @@
+export function shuffleArray(arr) {
+    let currentIndex = arr.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = arr[currentIndex];
+        arr[currentIndex] = arr[randomIndex];
+        arr[randomIndex] = temporaryValue;
+    }
+}
